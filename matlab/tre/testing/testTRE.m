7d7c1a1c35f94cdef825ae5ecd4c81bc95e743e4
Andrew Wiles <awiles@ndigital.com>
2013-05-07 15:18:26 -0400

    Added non-homogenous testing to TRE tests.
    TRE test script now contains a collection of non-homogenous testing and
    also some basic controls to enumerate the testing properly and add
    verbosity.

--- matlab/tre/testing/testTRE.m
+++ matlab/tre/testing/testTRE.m
@@ -1,17 +1,36 @@
-clear all;
+function pass = testTRE(varargin)
+testnum = 0;
+verbose = 0;
+pass = 1;
+
+if(nargin > 0)
+    nVarArgs = length(varargin);
+    i = 1;
+    while( i <= nVarArgs)
+        if( strcmp(varargin{i},'StartTestNumber'))
+            i=i+1;
+            testnum = varargin{i};
+        elseif( strcmp(varargin{i}, 'Verbose'))
+            verbose = 1;
+        else
+            error('Unknown parameter: %s', varargin{i})
+        end
+    end
+end
 
 % define West tool design parameters.
 A = 71;
 B = 54;
 rho = 170;
-
-testnum = 0;
 designs = {'d', 'e'};
 
+%% test the anisotropic formula with isotropic FLE.
+% define an arbitrary FLE error value where each axis is identical.
+FLE = 0.33;
 for i = 1:length(designs)
     testnum = testnum +1;
-    testname = sprintf('calcTRE.m Isotropic FLE, West Tool Design %s', designs{i});
-    fprintf( 'Test #%d: %s started...\n', testnum, testname);
+    testname = sprintf('calcTRE.m Isotropic FLE, Anisotropic Formula, West Tool Design %s', designs{i});
+    fprintf( 'Test #%d: %s started...', testnum, testname);
     % define the rigid body defintion in local probe coordinates.
     % West design in Fig. 2(e).
     [mrk, normals, tip] = getWestToolDesign(designs{i}, A, B, rho);
@@ -23,27 +42,66 @@ for i = 1:length(designs)
     mrk = (R * mrk')';
     tip = (R * tip')';
     
-    % define an arbitrary FLE error value where each axis is identical.
-    FLE = 0.33;
-    
     % using West's formula, compute the TRE.
     [TRE_West, Sigma_West, SigmaPA_West] = calcTRE(FLE,[mrk;tip],'FigNum', 1, 'Isotropic');
     % using my anisotropic formula with isotropic --> is it the same?
     [TRE_Wiles, Sigma_Wiles, SigmaPA_Wiles] = calcTRE(FLE,[mrk;tip], 'FigNum', 2);
     
     if( abs(TRE_Wiles - TRE_West) < 0.0001)
-        fprintf('\tTest #%d: %s passed.\n', testnum, testname);
+        fprintf(' passed.\n');
     else
-        fprintf('\tTest #%d: %s FAILED.\n', testnum, testname);
+        fprintf(' FAILED.\n');
+        pass = 0;
     end
-    fprintf('\t\tTRE West  = %3.4f\n', TRE_West);
-    fprintf('\t\tTRE_Wiles = %3.4f\n', TRE_Wiles);
+    if(verbose)
+        fprintf('\t\tTRE West  = %3.4f\n', TRE_West);
+        fprintf('\t\tTRE_Wiles = %3.4f\n', TRE_Wiles);
+    end
 end
+
+%% test the non-homogenous formula with isotropic FLE.
+% define an arbitrary FLE error value where each axis is identical.
+FLE = 0.33;
+% build the non-homogenous matrix.
+sigma0 = (FLE^2)/3 * eye(3);
+sigma = repmat(sigma0, [1,1,length(mrk)]);
+for i = 1:length(designs)
+    testnum = testnum +1;
+    testname = sprintf('calcTRE.m Isotropic FLE, Non-Homogenous Formula, West Tool Design %s', designs{i});
+    fprintf( 'Test #%d: %s started...', testnum, testname);
+    % define the rigid body defintion in local probe coordinates.
+    % West design in Fig. 2(e).
+    [mrk, normals, tip] = getWestToolDesign(designs{i}, A, B, rho);
+    
+    % now that the probe is created, let's rotate the probe into a useful
+    % orientation.
+    rotZ = 0;
+    R = getRotMatrixd([0, 0, rotZ]);
+    mrk = (R * mrk')';
+    tip = (R * tip')';
+        
+    % using West's formula, compute the TRE.
+    [TRE_West, Sigma_West, SigmaPA_West] = calcTRE(FLE,[mrk;tip],'FigNum', 1, 'Isotropic');
+    % using my anisotropic formula with isotropic --> is it the same?
+    [TRE_Wiles, Sigma_Wiles, SigmaPA_Wiles] = calcTRE(sigma,[mrk;tip], 'FigNum', 2);
+    
+    if( abs(TRE_Wiles - TRE_West) < 0.0001)
+        fprintf(' passed.\n');
+    else
+        fprintf(' FAILED.\n');
+        pass = 0;
+    end
+    if(verbose)
+        fprintf('\t\tTRE West  = %3.4f\n', TRE_West);
+        fprintf('\t\tTRE_Wiles = %3.4f\n', TRE_Wiles);
+    end
+end
+
 
 %% let's look at the TRE with the RB in different orientations.
 testnum = testnum +1;
 testname = 'calcTRE.m Various FLE, West Tool Design e, Different Orientations';
-fprintf( 'Test #%d: %s started...\n', testnum, testname);
+fprintf( 'Test #%d: %s started...', testnum, testname);
 ang = -45:1:45;
 TRE_iso_iso = zeros(size(ang));
 TRE_iso_ani = zeros(size(ang));
@@ -91,4 +149,4 @@ legend('Isotropic \Sigma_{fle}, Isotropic Formula',...
     'Anisotropic \Sigma_{fle}, Isotropic Formula',...
     'Anisotropic \Sigma_{fle}, Anisotropic Formula');
 title('Comparison of TRE Model using Isotropic and Anisotropic FLE and Formulas');
-fprintf('\tTest #%d: %s passed.\n', testnum, testname);
+fprintf(' passed.\n');
