781b635b5085c4347fa63438f420478641eafe95
Andrew Wiles <awiles@ndigital.com>
2013-05-10 11:59:52 -0400

    Changed email setup file to a generic smtp and email address.

--- matlab/email_setup.m
+++ matlab/email_setup.m
@@ -1,14 +1,5 @@
+function email_setup(smtp, email)
 % email set up.
 
-setpref('Internet','SMTP_Server','smtp.me.com');
-setpref('Internet','E_mail','awiles@me.com');
-
-%test
-% clear msg;
-% datetime = clock;
-% filename = sprintf('freespace_%4d%02d%02d_%02d%02d', datetime(1:5));
-% 
-% msg = sprintf('Model computing complete.\n    Started:  %d-%02d-%02d %02d:%02d\n    Ended:    %d-%02d-%02d %02d:%02d\n    Filename: %s',...
-%     datetime(1:5), datetime(1:5), filename);
-% 
-% sendmail('awiles@imaging.robarts.ca','FEMLAB Processing Complete',msg, 'readme.txt');
+setpref('Internet','SMTP_Server',smtp);
+setpref('Internet','E_mail',email);
