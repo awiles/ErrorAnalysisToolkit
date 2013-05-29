c4f40e2a134e6cae8d16a383bdb2a827c1505703
Andrew Wiles <awiles@ndigital.com>
2013-05-29 09:08:46 -0400

    Added max error and a rough point plot.

--- matlab/stats/get3DErrorStats.m
+++ matlab/stats/get3DErrorStats.m
@@ -21,12 +21,24 @@ errDist = (sum(errPos.^2,2)).^(.5);
 meanErr = mean([errPos errDist]);
 stdErr  = std([errPos errDist]);
 rmsErr  = sqrt(mean([errPos errDist].^2));
+maxErr  = max([errPos errDist]);
 covErr = cov(errPos);
 
-fprintf('\tRMS\t\t\tMean\t\tSt.Dev\t\n');
-fprintf('x\t%f\t%f\t%f\n', rmsErr(1), meanErr(1), stdErr(1) );
-fprintf('y\t%f\t%f\t%f\n', rmsErr(2), meanErr(2), stdErr(2) );
-fprintf('z\t%f\t%f\t%f\n', rmsErr(3), meanErr(3), stdErr(3) );
-fprintf('3D\t%f\t%f\t%f\n', rmsErr(4), meanErr(4), stdErr(4) );
+fprintf('\tRMS\t\tMean\tSt.Dev\tMax\t\n');
+fprintf('x\t%3.2f\t%3.2f\t%3.2f\t%3.2f\n', rmsErr(1), meanErr(1), stdErr(1), maxErr(1) );
+fprintf('y\t%3.2f\t%3.2f\t%3.2f\t%3.2f\n', rmsErr(2), meanErr(2), stdErr(2), maxErr(2) );
+fprintf('z\t%3.2f\t%3.2f\t%3.2f\t%3.2f\n', rmsErr(3), meanErr(3), stdErr(3), maxErr(3) );
+fprintf('3D\t%3.2f\t%3.2f\t%3.2f\t%3.2f\n', rmsErr(4), meanErr(4), stdErr(4), maxErr(4) );
 
-xfrm
+if( nargin > 2)
+    if( bFit )
+        fprintf('The data was registered for a best fit set of statistics.\n');
+        fprintf('    Xfrm: %3.6f, %3.6f, %3.6f, %3.6f, %3.2f, %3.2f, %3.2f\n', ...
+            xfrm.rot, xfrm.pos);
+    end
+end
+
+plot3(refPos0(:,1), refPos0(:,2), refPos0(:,3), '.k');
+hold on;
+plot3(measPos(:,1), measPos(:,2), measPos(:,3), '.r');
+hold off;
